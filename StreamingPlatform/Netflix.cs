2472de581ed4fc39f5affa7f0da9c99cab48580f
Fausto Boshoff <fausto.boshoff@gmail.com>
2023-04-20 12:52:15 +0200

    implemented netflix reproduction (changed bg color of playing films text)

--- StreamingPlatform/Netflix.cs
+++ StreamingPlatform/Netflix.cs
@@ -5,10 +5,10 @@
         public Netflix()
         {
             files = new NetflixVideo[] {
-                new NetflixVideo(){ title ="Titanic"},
-                new NetflixVideo(){ title ="Terminator"},
-                new NetflixVideo(){ title ="Alien", },
-                new NetflixVideo(){ title ="Avatar"}
+                new NetflixVideo(){ title ="Titanic",Color = System.ConsoleColor.Blue},
+                new NetflixVideo(){ title ="Terminator",Color = System.ConsoleColor.Cyan},
+                new NetflixVideo(){ title ="Alien",Color = System.ConsoleColor.Green },
+                new NetflixVideo(){ title ="Avatar",Color = System.ConsoleColor.Yellow}
 
                 };
 
