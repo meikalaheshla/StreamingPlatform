1854a21d1326549782fb44d66432fea53090fd34
Fausto Boshoff <fausto.boshoff@gmail.com>
2023-04-20 12:37:45 +0200

    Aggiungere i file di progetto.

---
+++ StreamingPlatform/Netflix.cs
@@ -0,0 +1,25 @@
+﻿namespace StreamingPlayer
+{
+    public sealed class Netflix : StreamingPlatform
+    {
+        public Netflix()
+        {
+            files = new NetflixVideo[] {
+                new NetflixVideo(){ title ="Titanic"},
+                new NetflixVideo(){ title ="Terminator"},
+                new NetflixVideo(){ title ="Alien", },
+                new NetflixVideo(){ title ="Avatar"}
+
+                };
+
+            totalTacks = files.Length;
+        }
+        private class NetflixVideo : Video
+        {
+            public NetflixVideo()
+            {
+            }
+        }
+    }
+   
+}
